dd901b2760e04e4d1ea61749f19efea2a15da76f
Mats070 <102987097+Mats070@users.noreply.github.com>
2023-02-26 22:19:02 +0100

    Add files via upload

--- routes/openLib.js
+++ routes/openLib.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", (req, res)=>{
     res.render("CalendarLibraryOverview",{
@@ -24,7 +26,7 @@ router.get("/js/cookieBar.js", (req, res)=>{
     const options = {
         root: __dirname + "/lib/js/"
     };
-    var fileName = 'OrganizeeCalendar.js';
+    var fileName = 'cookieBar.js';
     res.sendFile(fileName, options, function (err) {
         if (err) {
             res.sendStatus("500")
@@ -32,11 +34,12 @@ router.get("/js/cookieBar.js", (req, res)=>{
     });
 })
 
-router.get("/code/:name", (req, res)=>{
+router.get("/code/Calendar.css", (req, res)=>{
     const options = {
-        root: __dirname + "/lib/js/"
+        root: __dirname + "/lib/css/"
     };
-    var fileName = 'cookieBar.js';
+     console.log(options);
+    var fileName = 'OrganizeeCalendar.css';
     res.sendFile(fileName, options, function (err) {
         if (err) {
             res.sendStatus("500")
