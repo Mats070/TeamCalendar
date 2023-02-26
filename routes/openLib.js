fba08805703f679a84ac51297e9adb309ab3e1c2
Mats070 <102987097+Mats070@users.noreply.github.com>
2023-02-26 21:52:40 +0100

    Cookie Banner

--- routes/openLib.js
+++ routes/openLib.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const res = require("express/lib/response");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", (req, res)=>{
     res.render("CalendarLibraryOverview",{
@@ -21,7 +22,12 @@ router.get("/code/Calendar.js", (req, res)=>{
     });
 })
 
-router.get("/code/Calendar.css", (req, res)=>{
+router.get("/js/:name", (req, res)=>{
+    const filepath = path.join(process.cwd(), "routes", "lib", "js", req.params.name);
+    res.sendFile(filepath);
+})
+
+router.get("/code/:name", (req, res)=>{
     const options = {
         root: __dirname + "/lib/css/"
     };
